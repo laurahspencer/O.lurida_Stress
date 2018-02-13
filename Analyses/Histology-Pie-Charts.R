e8d319fb4870e4687d77b523be9472ed3b5c97b4
laurahspencer <lhs3@uw.edu>
2018-02-12 20:21:45 -0800

    updated contingency tables

--- Analyses/Histology-Pie-Charts.R
+++ Analyses/Histology-Pie-Charts.R
@@ -71,100 +71,94 @@ for (i in 1:length(Pops)) {
 library("gplots")
 All.6.Gonad.Stages <- rbind(All.6, All.6.Amb, All.6.Low)
 CT.Sex <- table(All.6.Gonad.Stages$Treatment, All.6.Gonad.Stages$Sex)
+CT.Sex.postOA <- table(subset(All.6.Gonad.Stages, Phase %in% "OA")$Treatment, subset(All.6.Gonad.Stages, Phase %in% "OA")$Sex)
 CT.stage <- table(All.6.Gonad.Stages$Treatment, All.6.Gonad.Stages$Stage)
+CT.stage.postOA <- table(subset(All.6.Gonad.Stages, Phase %in% "OA")$Treatment, subset(All.6.Gonad.Stages, Phase %in% "OA")$Stage)
 CT.Sex <- CT.Sex[-1:-3,]
 CT.stage <- CT.stage[-1:-3,]
+CT.Sex.postOA <- CT.Sex.postOA[-1:-4,]
+CT.stage.postOA <- CT.stage.postOA[-1:-4,]
 
 balloonplot(CT.Sex, main ="Oly Sex", xlab ="", ylab="",
             label = T, show.margins = FALSE)
 balloonplot(CT.stage, main ="Oly Stage", xlab ="", ylab="",
             label = T, show.margins = FALSE)
-
-library("graphics")
-mosaicplot(CT.Sex, shade = TRUE, las=2, main = "Oly Sex")
-mosaicplot(CT.stage, shade = TRUE, las=2, main = "Oly Stage")
+balloonplot(CT.Sex.postOA, main ="Oly Sex, post OA only \np-value = 0.8784", xlab ="", ylab="",
+            label = T, show.margins = FALSE)
+balloonplot(CT.stage.postOA, main ="Oly Stage, post OA only \nX-squared = 13.491, df = 2, p-value = 0.001176", xlab ="", ylab="",
+            label = T, show.margins = FALSE)
 
 # Stats on Sex data
 chisq.sex <- chisq.test(CT.Sex) #p-value = 0.2932; also I read that the warning message may be a result of little data.
 fisher.test(CT.Sex) #p-value = 0.2942, using Fisher's Exact Test for Count Data 
+fisher.test(CT.Sex.postOA) #p-value = 0.8784
 
 # Stats on Stage data
 chisq.stage <- chisq.test(CT.stage) #p-value = 0.002478
 fisher.test(CT.stage) #p-value = 0.003477 #using this test too, since I had to use it for the Sex data. 
-
-chisq.stage$observed
-chisq.stage$expected
-round(chisq.stage$residuals, 3)
+chisq.stage.postOA <- chisq.test(CT.stage.postOA) #p-value = 0.001176
+fisher.test(CT.stage.postOA) #p-value = 0.0007534
 
 install.packages("corrplot")
 library(corrplot)
 corrplot(chisq.stage$residuals, is.corr = F, main="Oly Stage, Correlation Plot")
 corrplot(chisq.sex$residuals, is.corr = F, main="Oly Sex, Correlation Plot")
+corrplot(chisq.stage.postOA$residuals, is.corr = F, main="Oly Stage - Post OA Only, Correlation Plot")
 
 # Run same stats on each population separately: 
-SN.6 <- subset(All.6.Gonad.Stages, Population %in% "SN")
-NF.6 <- subset(All.6.Gonad.Stages, Population %in% "NF")
-K.6 <- subset(All.6.Gonad.Stages, Population %in% "K")
-HL.6 <- subset(All.6.Gonad.Stages, Population %in% "HL")
+SN.6.postOA <- subset(All.6.Gonad.Stages, Population %in% "SN" & Phase %in% "OA")
+NF.6.postOA <- subset(All.6.Gonad.Stages, Population %in% "NF" & Phase %in% "OA")
+K.6.postOA <- subset(All.6.Gonad.Stages, Population %in% "K" & Phase %in% "OA")
+HL.6.postOA <- subset(All.6.Gonad.Stages, Population %in% "HL" & Phase %in% "OA")
 
 # Contingency tables for each population, by Sex and Stage
-CT.SN.Sex <- table(SN.6$Treatment, SN.6$Sex)
-CT.SN.stage <- table(SN.6$Treatment, SN.6$Stage)
-CT.NF.Sex <- table(NF.6$Treatment, NF.6$Sex)
-CT.NF.stage <- table(NF.6$Treatment, NF.6$Stage)
-CT.K.Sex <- table(K.6$Treatment, K.6$Sex)
-CT.K.stage <- table(K.6$Treatment, K.6$Stage)
-CT.HL.Sex <- table(HL.6$Treatment, HL.6$Sex)
-CT.HL.stage <- table(HL.6$Treatment, HL.6$Stage)
+CT.SN.Sex <- table(SN.6.postOA$Treatment, SN.6.postOA$Sex)
+CT.SN.stage <- table(SN.6.postOA$Treatment, SN.6.postOA$Stage)
+CT.NF.Sex <- table(NF.6.postOA$Treatment, NF.6.postOA$Sex)
+CT.NF.stage <- table(NF.6.postOA$Treatment, NF.6.postOA$Stage)
+CT.K.Sex <- table(K.6.postOA$Treatment, K.6.postOA$Sex)
+CT.K.stage <- table(K.6.postOA$Treatment, K.6.postOA$Stage)
+CT.HL.Sex <- table(HL.6.postOA$Treatment, HL.6.postOA$Sex)
+CT.HL.stage <- table(HL.6.postOA$Treatment, HL.6.postOA$Stage)
 
 # Remove extraneous rows
-CT.SN.Sex <- CT.SN.Sex[-1:-3,]
-CT.SN.stage <- CT.SN.stage[-1:-3,]
-CT.NF.Sex <- CT.NF.Sex[-1:-3,]
-CT.NF.stage <- CT.NF.stage[-1:-3,]
-CT.K.Sex <- CT.K.Sex[-1:-3,]
-CT.K.stage <- CT.K.stage[-1:-3,]
-CT.HL.Sex <- CT.HL.Sex[-1:-3,]
-CT.HL.stage <- CT.HL.stage[-1:-3,]
-
-chisq.test(CT.SN.Sex)
-chisq.test(CT.SN.stage)
-chisq.test(CT.NF.Sex)
-chisq.test(CT.NF.stage)
-chisq.test(CT.K.Sex)
-chisq.test(CT.K.stage)
-chisq.test(CT.HL.Sex)
-chisq.test(CT.HL.stage)
+CT.SN.Sex <- CT.SN.Sex[-1:-4,]
+CT.SN.stage <- CT.SN.stage[-1:-4,]
+CT.NF.Sex <- CT.NF.Sex[-1:-4,]
+CT.NF.stage <- CT.NF.stage[-1:-4,]
+CT.K.Sex <- CT.K.Sex[-1:-4,]
+CT.K.stage <- CT.K.stage[-1:-4,]
+CT.HL.Sex <- CT.HL.Sex[-1:-4,]
+CT.HL.stage <- CT.HL.stage[-1:-4,]
 
 # Sample size small, so use fisher's test
 fisher.test(CT.SN.Sex)
-fisher.test(CT.SN.stage)
-fisher.test(CT.NF.Sex)
-fisher.test(CT.NF.stage)
 fisher.test(CT.K.Sex)
-fisher.test(CT.K.stage)
+fisher.test(CT.NF.Sex)
 fisher.test(CT.HL.Sex)
+
+fisher.test(CT.SN.stage)
+fisher.test(CT.K.stage)
+fisher.test(CT.NF.stage)
 fisher.test(CT.HL.stage)
 
-
-balloonplot(CT.SN.Sex, main ="Oly Sex, South Sound F1", xlab ="", ylab="",
+balloonplot(CT.SN.Sex, main ="Oly Sex, South Sound F1 \np-value = 1", xlab ="", ylab="",
             label = T, show.margins = FALSE)
-balloonplot(CT.SN.stage, main ="Oly Stage, South Sound F1", xlab ="", ylab="",
+balloonplot(CT.K.Sex, main ="Oly Sex, South Sound F2 \np-value = 0.2164", xlab ="", ylab="",
             label = T, show.margins = FALSE)
-
-balloonplot(CT.NF.Sex, main ="Oly Sex, North Sound F1", xlab ="", ylab="",
+balloonplot(CT.NF.Sex, main ="Oly Sex, North Sound F1 \np-value = 0.2629", xlab ="", ylab="",
             label = T, show.margins = FALSE)
-balloonplot(CT.NF.stage, main ="Oly Stage, North Sound F1", xlab ="", ylab="",
+balloonplot(CT.HL.Sex, main ="Oly Sex, Hood Canal F1 \np-value = 0.4545", xlab ="", ylab="",
             label = T, show.margins = FALSE)
 
-balloonplot(CT.K.Sex, main ="Oly Sex, South Sound F2", xlab ="", ylab="",
-            label = T, show.margins = FALSE)
-balloonplot(CT.K.stage, main ="Oly Stage, South Sound F2", xlab ="", ylab="",
-            label = T, show.margins = FALSE)
 
-balloonplot(CT.HL.Sex, main ="Oly Sex, Hood Canal F1", xlab ="", ylab="",
+balloonplot(CT.SN.stage, main ="Oly Stage, South Sound F1 \np-value = 0.2542", xlab ="", ylab="",
             label = T, show.margins = FALSE)
-balloonplot(CT.HL.stage, main ="Oly Stage, Hood Canal F1", xlab ="", ylab="",
+balloonplot(CT.K.stage, main ="Oly Stage, South Sound F2 \np-value = 0.04611", xlab ="", ylab="",
+            label = T, show.margins = FALSE)
+balloonplot(CT.NF.stage, main ="Oly Stage, North Sound F1 \np-value = 0.1399", xlab ="", ylab="",
+            label = T, show.margins = FALSE)
+balloonplot(CT.HL.stage, main ="Oly Stage, Hood Canal F1 \np-value = 0.2857", xlab ="", ylab="",
             label = T, show.margins = FALSE)
 
 ### boneyard
