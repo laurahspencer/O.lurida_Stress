27c7820991ac9774b1ab13c358bbdfd9ea63e6ee
laurahspencer <lhs3@uw.edu>
2018-06-07 22:53:24 -0700

    added images

--- Analyses/Bucket-Densities.R
+++ Analyses/Bucket-Densities.R
@@ -1,15 +1,12 @@
-setwd("~/Documents/Roberts Lab/O.lurida_Stress")
 Bucket.Densities <- read.csv("Data/Bucket-Densities.csv", header = T, stringsAsFactors = F, na.strings = " -   ")
 library(plotly)
 library(reshape2)
-as.numeric(Bucket.Densities[,-1])
-test <- as.numeric(as.character(unlist(Bucket.Densities[,-1])), na.action=na.omit)
-str(Bucket.Densities[,-1])
+colnames(Bucket.Densities) <- sub(".Dens", "", colnames(Bucket.Densities))
 
-Bucket.Densities.long <- melt(Bucket.Densities, id.vars = "Date") 
+#as.numeric(Bucket.Densities[,-1])
+#test <- as.numeric(as.character(unlist(Bucket.Densities[,-1])), na.action=na.omit)
 
-# Groups with 2 larval buckets:  
-# SN10-ambient, SN10-Low
+# Groups with 2 larval buckets, and times:  
 # NF10-Low, started new bucket on 5/24 
 # NF10-Ambient, started new bucket on 5/25
 # NF6-Low, started new bucket on 5/25
@@ -18,6 +15,15 @@ Bucket.Densities.long <- melt(Bucket.Densities, id.vars = "Date")
 # SN10-Ambient, started new bucket on 6/4
 
 # As of 6/9, only 1 bucket per treatment group in bucket counts, so on previous screening day (6/5) conjoined all 2-bucket systems. 
+
+Bucket.Densities$NF10Low.b
+
+
+
+head(Bucket.Densities)
+
+Bucket.Densities.long <- na.omit(melt(Bucket.Densities, id.vars = "Date"))
+head(Bucket.Densities.long)
 
 Density.plot <- plot_ly(data = Bucket.Densities.long, x = ~Date, y = ~value, type="scatter", mode="lines", color=~variable, hovertext=~value) %>%  #generate plotly plot
   layout(title="2017 O. lurida Bucket Densities",
